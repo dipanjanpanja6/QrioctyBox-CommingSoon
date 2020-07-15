3e905553ad1b50526c6b26a365285445154427cb
D1p4Nj4Np4Nj4 <dipanjanpanja6@noreply.github.com>
2020-07-15 17:58:25 +0530

    build

---
+++ build/precache-manifest.a9b5dd94b100cb7168a8b34d4cb8ca1b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa64c6bdc45107b218ca9f33ddcdfba3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b95338f5c77a230f7c41",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "cd78dff6eb5f04af0c88",
+    "url": "/static/css/main.ab9b8957.chunk.css"
+  },
+  {
+    "revision": "b95338f5c77a230f7c41",
+    "url": "/static/js/2.7a5ea4bd.chunk.js"
+  },
+  {
+    "revision": "75304a605f9c03e664948edc1c5186bb",
+    "url": "/static/js/2.7a5ea4bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd78dff6eb5f04af0c88",
+    "url": "/static/js/main.e802f6ed.chunk.js"
+  },
+  {
+    "revision": "2aa9a407498d95ea9b76",
+    "url": "/static/js/runtime-main.6e45c9c4.js"
+  },
+  {
+    "revision": "ab1af16801d2097ad968d8d3d8d083d1",
+    "url": "/static/media/BigLogo.ab1af168.svg"
+  },
+  {
+    "revision": "56237c86941fd8af63c0b603a5f12539",
+    "url": "/static/media/BigLogoText.56237c86.png"
+  },
+  {
+    "revision": "6255f8b8605743cbff50c32d65cab055",
+    "url": "/static/media/Team.6255f8b8.svg"
+  },
+  {
+    "revision": "558cf3a0350ff266b53267e24184be1e",
+    "url": "/static/media/Wallman-Bold-free.558cf3a0.ttf"
+  },
+  {
+    "revision": "9c90dd070da86cdcd6fa9b32835603a5",
+    "url": "/static/media/q+.9c90dd07.svg"
+  }
+]);
